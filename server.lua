33eef784a41b78031c3bc67e7822b390cab7e548
CodeForge <125872426+C0deForge@users.noreply.github.com>
2024-10-23 11:28:41 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -18,26 +18,30 @@ end
 -- Event to set the DEFCON level
 RegisterNetEvent('defcon:setLevel')
 AddEventHandler('defcon:setLevel', function(level)
-    currentDefconLevel = level
-    TriggerClientEvent('defcon:notifyAllPlayers', -1, level)
-    TriggerClientEvent('defcon:showUI', -1, level)
-end)
-
--- Event to clear the DEFCON level
-RegisterNetEvent('defcon:clearLevel')
-AddEventHandler('defcon:clearLevel', function()
-    if currentDefconLevel == 0 then
-        local message = Config.Messages.NoActiveDEFCON
-        if ESX then
-            local xPlayer = ESX.GetPlayerFromId(source)
-            xPlayer.showNotification(message)
-        elseif QBCore then
-            TriggerClientEvent('QBCore:Notify', source, message, "error")
+    if level == 0 then
+        if currentDefconLevel == 0 then
+            -- Notify only the player who tried to deactivate DEFCON
+            local message = Config.Messages.NoActiveDEFCON
+            if ESX then
+                local xPlayer = ESX.GetPlayerFromId(source)
+                if xPlayer then
+                    xPlayer.showNotification(message)
+                end
+            elseif QBCore then
+                -- Notify only the player who triggered the event in QBCore
+                TriggerClientEvent('QBCore:Notify', source, message, "error")
+            end
+        else
+            -- DEFCON is active, clear it for all players
+            currentDefconLevel = 0
+            TriggerClientEvent('defcon:notifyAllPlayers', -1, 0) -- Notify all players that DEFCON is deactivated
+            TriggerClientEvent('defcon:clearUI', -1) -- Clear the UI for all players
         end
     else
-        currentDefconLevel = 0
-        TriggerClientEvent('defcon:notifyAllPlayers', -1, 0) -- Send 0 instead of 'remove'
-        TriggerClientEvent('defcon:clearUI', -1) -- Clear the UI for all players
+        -- Set new DEFCON level
+        currentDefconLevel = level
+        TriggerClientEvent('defcon:notifyAllPlayers', -1, level)
+        TriggerClientEvent('defcon:showUI', -1, level)
     end
 end)
 
